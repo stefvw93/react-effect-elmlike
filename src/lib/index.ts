28554d7943e3a66a994ae918c2745614296edc9a
stefvw93 <stef@stefvanwijchen.com>
2026-07-29 22:55:42 +0200

    things

---
+++ src/lib/index.ts
@@ -0,0 +1,1 @@
+export * as Program from "./program";
---
+++ src/main.tsx
@@ -0,0 +1,48 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Effect, Schema } from "effect";
+import { Program } from "./lib";
+
+const App = Program.make({
+  model: Schema.Struct({
+    count: Schema.Number,
+  }),
+
+  message: [
+    Schema.TaggedStruct("Inc", { by: Schema.Number }),
+    Schema.TaggedStruct("Dec", { by: Schema.Number }),
+    Schema.TaggedStruct("Reset", {}),
+  ],
+
+  view: (model, dispatch) => (
+    <main>
+      <div>
+        <button type="button" onClick={() => dispatch({ _tag: "Dec", by: 1 })}>
+          - 1
+        </button>
+        <output>count is {model.count}</output>
+        <button type="button" onClick={() => dispatch({ _tag: "Inc", by: 1 })}>
+          + 1
+        </button>
+      </div>
+
+      <button type="button" onClick={() => dispatch({ _tag: "Reset" })}>
+        reset
+      </button>
+    </main>
+  ),
+}).pipe((program) =>
+  Program.init(program, {
+    update: {
+      Inc: (msg, model) => Effect.succeed({ count: model.count + msg.by }),
+      Dec: (msg, model) => Effect.succeed({ count: model.count - msg.by }),
+      Reset: () => Effect.succeed({ count: 0 }),
+    },
+  }),
+);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App count={0} />
+  </StrictMode>,
+);
